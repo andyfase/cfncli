05cde62b7486774766406ef43b3b339aca9f8cf2
Andy Fase <andrewfase@gmail.com>
2023-12-04 09:44:24 -0800

    Set to 0.1.0 for release

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     name='cfncli',
 
     # Version and description
-    version='0.0.7',
+    version='0.1.0',
     description='CloudFormation CLI Wrapper',
     long_description=long_description,
     long_description_content_type='text/markdown',
