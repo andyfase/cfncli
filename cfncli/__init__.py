b91f81f0953a522ba5707dbe91ffe7f1b8ac4f16
Andy Fase <andrfase@amazon.com>
2025-07-29 10:08:29 -0700

    move to importlib.metadata vs pkg_resources.require which is depreciated

--- cfncli/__init__.py
+++ cfncli/__init__.py
@@ -1,9 +1,8 @@
 """cfncli version."""
 
-import pkg_resources
+from importlib.metadata import version, PackageNotFoundError
 
 try:
-  packages = pkg_resources.require('cfncli')
-  __version__ = packages[0].version if packages else "0.0"
-except pkg_resources.DistributionNotFound: ## local development
+  __version__ = version('cfncli')
+except PackageNotFoundError:  ## local development
   __version__ = "0.0"
