5f12503c226c70615fe3b7108da3642841a3c7c5
Andy Fase <andrewfase@gmail.com>
2025-09-12 09:52:37 -0700

    chore: linting

--- tests/test_ordering.py
+++ tests/test_ordering.py
@@ -9,12 +9,13 @@ import re
 
 logger = logging.getLogger(__name__)
 
+
 def generate_stack_regex(stacks):
-  regex = ".*"
-  for stack in stacks:
-    regex += f"Deploying stack {stack}.*"
-  return regex
-    
+    regex = ".*"
+    for stack in stacks:
+        regex += f"Deploying stack {stack}.*"
+    return regex
+
 
 @pytest.mark.parametrize("get_config", ["multiple.yaml"], indirect=["get_config"])
 @mock_aws
@@ -25,13 +26,14 @@ def test_stack_order(cli_runner, get_config):
     # Change to temp directory
     original_cwd = os.getcwd()
     os.chdir(tmpdir)
-    regex = re.compile(generate_stack_regex(['NormalOrder1\.TestStack1', 'NormalOrder1\.TestStack2']), flags=re.DOTALL)
+    regex = re.compile(generate_stack_regex(["NormalOrder1\.TestStack1", "NormalOrder1\.TestStack2"]), flags=re.DOTALL)
     try:
         result = cli_runner.invoke(cli, ["-f", "cfn-cli.yaml", "-s", "NormalOrder1.*", "stack", "deploy"])
         assert result.exit_code == 0
         assert regex.match(result.output) is not None
     finally:
         os.chdir(original_cwd)
+
 
 @pytest.mark.parametrize("get_config", ["multiple.yaml"], indirect=["get_config"])
 @mock_aws
@@ -42,13 +44,16 @@ def test_stack_order_reverse(cli_runner, get_config):
     # Change to temp directory
     original_cwd = os.getcwd()
     os.chdir(tmpdir)
-    regex = re.compile(generate_stack_regex(['ReverseOrder1\.TestStack2', 'ReverseOrder1\.TestStack1']), flags=re.DOTALL)
+    regex = re.compile(
+        generate_stack_regex(["ReverseOrder1\.TestStack2", "ReverseOrder1\.TestStack1"]), flags=re.DOTALL
+    )
     try:
         result = cli_runner.invoke(cli, ["-f", "cfn-cli.yaml", "-s", "ReverseOrder1.*", "stack", "deploy"])
         assert result.exit_code == 0
         assert regex.match(result.output) is not None
     finally:
         os.chdir(original_cwd)
+
 
 @pytest.mark.parametrize("get_config", ["multiple.yaml"], indirect=["get_config"])
 @mock_aws
@@ -59,13 +64,14 @@ def test_stage_order(cli_runner, get_config):
     # Change to temp directory
     original_cwd = os.getcwd()
     os.chdir(tmpdir)
-    regex = re.compile(generate_stack_regex(['NormalOrder1\.TestStack1', 'NormalOrder2\.TestStack1']), flags=re.DOTALL)
+    regex = re.compile(generate_stack_regex(["NormalOrder1\.TestStack1", "NormalOrder2\.TestStack1"]), flags=re.DOTALL)
     try:
         result = cli_runner.invoke(cli, ["-f", "cfn-cli.yaml", "-s", "NormalOrder*.TestStack1", "stack", "deploy"])
         assert result.exit_code == 0
         assert regex.match(result.output) is not None
     finally:
         os.chdir(original_cwd)
+
 
 @pytest.mark.parametrize("get_config", ["multiple.yaml"], indirect=["get_config"])
 @mock_aws
@@ -76,13 +82,16 @@ def test_stage_order_reverse(cli_runner, get_config):
     # Change to temp directory
     original_cwd = os.getcwd()
     os.chdir(tmpdir)
-    regex = re.compile(generate_stack_regex(['ReverseOrder2\.TestStack1', 'ReverseOrder1\.TestStack1']), flags=re.DOTALL)
+    regex = re.compile(
+        generate_stack_regex(["ReverseOrder2\.TestStack1", "ReverseOrder1\.TestStack1"]), flags=re.DOTALL
+    )
     try:
         result = cli_runner.invoke(cli, ["-f", "cfn-cli.yaml", "-s", "ReverseOrder*.TestStack1", "stack", "deploy"])
         assert result.exit_code == 0
         assert regex.match(result.output) is not None
     finally:
         os.chdir(original_cwd)
+
 
 @pytest.mark.parametrize("get_config", ["multiple.yaml"], indirect=["get_config"])
 @mock_aws
@@ -93,13 +102,24 @@ def test_stage_and_stack_order(cli_runner, get_config):
     # Change to temp directory
     original_cwd = os.getcwd()
     os.chdir(tmpdir)
-    regex = re.compile(generate_stack_regex(['NormalOrder1\.TestStack1', 'NormalOrder1\.TestStack2', 'NormalOrder2\.TestStack1', 'NormalOrder2\.TestStack2']), flags=re.DOTALL)
+    regex = re.compile(
+        generate_stack_regex(
+            [
+                "NormalOrder1\.TestStack1",
+                "NormalOrder1\.TestStack2",
+                "NormalOrder2\.TestStack1",
+                "NormalOrder2\.TestStack2",
+            ]
+        ),
+        flags=re.DOTALL,
+    )
     try:
         result = cli_runner.invoke(cli, ["-f", "cfn-cli.yaml", "-s", "NormalOrder*.*", "stack", "deploy"])
         assert result.exit_code == 0
         assert regex.match(result.output) is not None
     finally:
         os.chdir(original_cwd)
+
 
 @pytest.mark.parametrize("get_config", ["multiple.yaml"], indirect=["get_config"])
 @mock_aws
@@ -110,11 +130,20 @@ def test_stage_and_stack_order_reverse(cli_runner, get_config):
     # Change to temp directory
     original_cwd = os.getcwd()
     os.chdir(tmpdir)
-    regex = re.compile(generate_stack_regex(['ReverseOrder2\.TestStack2', 'ReverseOrder2\.TestStack1', 'ReverseOrder1\.TestStack2', 'ReverseOrder1\.TestStack1']), flags=re.DOTALL)
+    regex = re.compile(
+        generate_stack_regex(
+            [
+                "ReverseOrder2\.TestStack2",
+                "ReverseOrder2\.TestStack1",
+                "ReverseOrder1\.TestStack2",
+                "ReverseOrder1\.TestStack1",
+            ]
+        ),
+        flags=re.DOTALL,
+    )
     try:
         result = cli_runner.invoke(cli, ["-f", "cfn-cli.yaml", "-s", "ReverseOrder*.*", "stack", "deploy"])
         assert result.exit_code == 0
         assert regex.match(result.output) is not None
     finally:
         os.chdir(original_cwd)
-
