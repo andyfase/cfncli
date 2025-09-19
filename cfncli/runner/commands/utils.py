be879ca315ba56e69e6984c74c342546d2681fa4
Andy Fase <andrewfase@gmail.com>
2025-09-19 16:42:50 -0700

    more lint

--- cfncli/runner/commands/utils.py
+++ cfncli/runner/commands/utils.py
@@ -7,6 +7,7 @@ def get_changeset_name(changeset_arn, shorten=True):
     # arn format for changeset arn:aws:cloudformation:region:account-id:changeSet/change-set-name/change-set-id
     return changeset_arn if not shorten else changeset_arn.split("/")[1]
 
+
 def update_termination_protection(session, termination_protection, stack_name, ppt):
     """Update termination protection on a stack"""
 
